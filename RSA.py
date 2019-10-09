643acdf5e661831a3fb7d9bce87609b1c5c057cf
Parker Watkins <parkergw@gmail.com>
2019-10-08 18:00:01 -0600

    GenerateKeys almost ready for testing

--- RSA.py
+++ RSA.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import random
 
+#TODO: unittests for toBase10, computeGCD, findRelativePrime, egcd
+
 # Write a class called RSA
 class RSA:
     def __init__(self):
@@ -9,12 +11,15 @@ class RSA:
     
     # Write a method called GenerateKeys that takes two very long text strings as input.
     def GenerateKeys(self, str1, str2):
+        str1 = str1.replace(" ", "").lower()
+        str2 = str2.replace(" ", "").lower()
+
         p = self.toBase10(str1, 26)
         q = self.toBase10(str2, 26)
         #mod by 10^200 to make them the right size. Ensure that they were longer than 10^200 before doing the mod, or print a Warning message.
-        if num1 < 10**200:
+        if p < 10**200:
             print("WARNING: number 1 is too small")
-        if num2 < 10**200:
+        if q < 10**200:
             print("WARNING: number 2 is too small")
         else:
             #Make them odd. Then start adding 2 until they are prime.
@@ -28,14 +33,18 @@ class RSA:
                 p += 2
             while not self.isPrimeMiller(q):
                 q += 2
+
             #Now you have your two, 200 digit prime numbers, p and q.
             #Calculate n = p*q
             n = p * q
             #Calculate r = (p-1)*(q-1)
             r = (p - 1)*(q - 1)
             #Find e – a 398 digit number that is relatively prime with r.
-            
-            
+            e = self.findRelativePrime(r)
+
+            #Find d – the inverse of e mod r.
+            gcd, d, z = self.egcd(e, r)
+
     def toBase10(self, s, base):
         alphabet = "0123456789abcdefghijklmnopqrstuvwxyz"
         a = 0
@@ -43,9 +52,28 @@ class RSA:
             value = alphabet.find(c)
             a *= base
             a += value
-        return a
-    
-    
+        return
+
+    def computeGCD(self, x, y):
+        while y:
+            x, y = y, x % y
+        return x
+
+    def findRelativePrime(self, n):
+        nextN = n + 2
+        while self.computeGCD(n, nextN)  != 1:
+            nextN += 2
+        return nextN
+
+    # Python program for Extended Euclidean algorithm
+    def egcd(self, a, b):
+        if a == 0:
+            return (b, 0, 1)
+        else:
+            gcd, x, y = self.egcd(b % a, a)
+            return (gcd, y - (b // a) * x, x)
+
+
     def millersTest(self, n):
         # Returns True if num is a prime number.
         t = n - 1
@@ -74,11 +102,19 @@ class RSA:
                 return False
         return True
     
-    
+
+quote1 = "Wizards First Rule people are stupid Richard and Kahlan frowned even more People are stupid given proper " \
+        "motivation almost anyone will believe almost anything Because people are stupid they will believe a lie " \
+        "because they want to believe its true or because they are afraid it might be true"
+
+quote2 = "A man will find a single coin in the mud and talk about it for days but when his inheritance comes and is" \
+         " accounted one percent less than he expected then he will declare himself cheated"
+
+
 def main():
     rsa = RSA()
-    num1, num2 = rsa.GenerateKeys('asdf', 'fdsa')
-    print(num1, num2)
+    num1, num2 = rsa.GenerateKeys(quote1, quote2)
+    print(num1,  "\n",  num2)
     # test to make sure the alphabet is mapped correctly
 #    alphabet = "0123456789abcdefghijklmnopqrstuvwxyz"
 #    for c in alphabet:
